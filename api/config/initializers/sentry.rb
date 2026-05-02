b0cef94ac998047038407e176d8ba195019d22f2
Marcus Reis <mail.marcus.reis@gmail.com>
2026-05-02 14:19:09 -0300

    add deploy pipeline

---
+++ api/config/initializers/sentry.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+Sentry.init do |config|
+  config.dsn = ENV["SENTRY_DSN"]
+
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  config.send_default_pii = false
+  config.environment = Rails.env
+end
